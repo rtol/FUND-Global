1baf89e2181c8251971ec8a4fe6591b225454858
Richard S.J. Tol <r.tol@sussex.ac.uk>
2024-01-02 12:02:17 +0000

    Add files via upload

---
+++ InitModules42.m
@@ -0,0 +1,16 @@
+%InitModules
+%The Climate Framework for Uncertainty, Negotiation and Distribution,
+%version 4.2-matlab-global
+%
+%This script is part of FUND 4.2 MG
+%It initializes variables and sets parameters
+%
+%Richard Tol, 27 July 2022
+%This code is protected by the MIT License
+
+initCO2
+initGHG
+initRF
+initClim
+initImpact42
+initKaya
